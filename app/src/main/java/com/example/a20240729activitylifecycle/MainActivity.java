8fa044e2a96e2ff1ee395981d498e3cf253e3d28
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-07-29 10:56:44 +0800

    v1.022

--- app/src/main/java/com/example/a20240729activitylifecycle/MainActivity.java
+++ app/src/main/java/com/example/a20240729activitylifecycle/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.a20240729activitylifecycle;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +11,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.net.URI;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        log.d("觀察方法","OnCreate Running");
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -21,4 +29,17 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    public void callMap(View view)
+    {
+        Intent i =new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse("geo:0?0q=FCU"));
+        startActivity(i);
+
+    }
+    public void finsihApp(View view)
+    {
+        finish();
+    }
+
 }
